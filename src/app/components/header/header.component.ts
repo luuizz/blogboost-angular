16aaee01d7468a17c566f93292a1908bf5155f3e
Luiz Ricardo <luu.izz@hotmail.com>
2025-04-01 23:21:17 -0300

    feat: estilizado header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { GridComponent } from '../grid/grid.component';
-import { SearchComponent } from './search/search.component';
 import { NavigationComponent } from './navigation/navigation.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [GridComponent, SearchComponent, NavigationComponent],
+  imports: [GridComponent, NavigationComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
