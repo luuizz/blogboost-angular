3481506930e60daf48673744fe0b4f59f5e084e6
Luiz Ricardo <luu.izz@hotmail.com>
2025-04-03 00:01:19 -0300

    feat: criado componente de item-post

---
+++ src/app/pages/home/sections/hero/item-post/item-post.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import { CaptionComponent } from '@components/caption/caption.component';
+import { CommonModule } from '@angular/common';
+import { Post } from '@shared/interfaces/post.interface';
+
+@Component({
+  selector: 'app-item-post',
+  imports: [CaptionComponent, CommonModule],
+  templateUrl: './item-post.component.html',
+  styleUrl: './item-post.component.scss',
+  standalone: true,
+})
+export class ItemPostComponent {
+  @Input() post!: Post;
+}
