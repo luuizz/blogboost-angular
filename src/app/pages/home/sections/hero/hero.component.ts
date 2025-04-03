8b6d7ffe3f142f495261afb5ff113c6f31dde359
Luiz Ricardo <luu.izz@hotmail.com>
2025-04-03 00:01:48 -0300

    feat: adicionado conteudo e componentes na hero section

--- src/app/pages/home/sections/hero/hero.component.ts
+++ src/app/pages/home/sections/hero/hero.component.ts
@@ -1,11 +1,45 @@
 import { Component } from '@angular/core';
-import { GridComponent } from '../../../../components/grid/grid.component';
+import { GridComponent } from '@components/grid/grid.component';
+import { CaptionComponent } from '@components/caption/caption.component';
+import { ItemPostComponent } from './item-post/item-post.component';
+import { CommonModule } from '@angular/common';
+import { Post } from '@shared/interfaces/post.interface';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [GridComponent],
+  imports: [GridComponent, CaptionComponent, ItemPostComponent, CommonModule],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss',
 })
-export class HeroComponent {}
+export class HeroComponent {
+  posts: Post[] = [
+    {
+      image: '/image-post-xs-01.jpg',
+      alt: 'Imagem do primeiro post',
+      category: 'tecnologia',
+      title: 'Você fica inseguro(a) na hora de escrever?',
+      date: '16, Fev',
+      readingTime: '12min de leitura',
+      link: '#',
+    },
+    {
+      image: '/image-post-xs-02.jpg',
+      alt: 'Imagem do segundo post',
+      category: 'tecnologia',
+      title: 'Você fica inseguro(a) na hora de escrever?',
+      date: '16, Fev',
+      readingTime: '12min de leitura',
+      link: '#',
+    },
+    {
+      image: '/image-post-xs-03.png',
+      alt: 'Imagem do terceiro post',
+      category: 'tecnologia',
+      title: 'Você fica inseguro(a) na hora de escrever?',
+      date: '16, Fev',
+      readingTime: '12min de leitura',
+      link: '#',
+    },
+  ];
+}
