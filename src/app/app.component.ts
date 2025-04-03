1bd4a8a24c56495dd4c0c1aa5d07a0f0edd909f6
Luiz Ricardo <luu.izz@hotmail.com>
2025-04-03 00:02:46 -0300

    fix: corrigido os caminhos e trocado para o alias

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from '@components/header/header.component';
+import { FooterComponent } from '@components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
