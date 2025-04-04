7ffbdb012f120659f1a2e14bc32e1168411d04db
Luiz Ricardo <luu.izz@hotmail.com>
2025-04-04 00:32:50 -0300

    feat: inicio da section

---
+++ src/app/pages/home/sections/all-posts/all-posts.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-all-posts',
+  imports: [],
+  templateUrl: './all-posts.component.html',
+  styleUrl: './all-posts.component.scss'
+})
+export class AllPostsComponent {
+
+}
---
+++ src/app/pages/home/sections/podcasts/podcasts.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-podcasts',
+  imports: [],
+  templateUrl: './podcasts.component.html',
+  styleUrl: './podcasts.component.scss'
+})
+export class PodcastsComponent {
+
+}
