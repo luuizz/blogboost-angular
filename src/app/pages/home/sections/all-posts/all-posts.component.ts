6c61b70eed6914961745421d521f7eecb6fc3720
Luiz Ricardo <luu.izz@hotmail.com>
2025-04-11 23:24:01 -0300

    feat: adicionado botão para query e corrigido loop

--- src/app/pages/home/sections/all-posts/all-posts.component.ts
+++ src/app/pages/home/sections/all-posts/all-posts.component.ts
@@ -11,60 +11,73 @@ import { Post } from '@shared/interfaces/post.interface';
   styleUrl: './all-posts.component.scss',
 })
 export class AllPostsComponent {
-  postsRecentes: Post[] = [
-    {
-      title: 'Realidade virtual: Entendendo a tecnologia',
-      date: '16, Fev',
-      readingTime: '12min',
-      category: 'Tecnologia',
-      image: '/img-post-topic-01.jpg',
-      alt: 'Imagem do post',
-      link: '#',
-    },
-    {
-      title: 'Você fica inseguro(a) na hora de escrever?',
-      date: '16, Fev',
-      readingTime: '12min',
-      category: 'Tecnologia',
-      image: '/img-post-topic-02.jpg',
-      alt: 'Imagem do post',
-      link: '#',
-    },
-    {
-      title: 'React Native Weekend - 21’ na minha visão',
-      date: '16, Fev',
-      readingTime: '12min',
-      category: 'Tecnologia',
-      image: '/img-post-topic-03.jpg',
-      alt: 'Imagem do post',
-      link: '#',
-    },
-    {
-      title: 'Como ir além apenas das linhas de código?',
-      date: '16, Fev',
-      readingTime: '12min',
-      category: 'Tecnologia',
-      image: '/img-post-topic-04.jpg',
-      alt: 'Imagem do post',
-      link: '#',
-    },
-    {
-      title: 'Como ir além apenas das linhas de código?',
-      date: '16, Fev',
-      readingTime: '12min',
-      category: 'Tecnologia',
-      image: '/img-post-topic-05.jpg',
-      alt: 'Imagem do post',
-      link: '#',
-    },
-    {
-      title: 'Você fica inseguro(a) na hora de escrever?',
-      date: '16, Fev',
-      readingTime: '12min',
-      category: 'Tecnologia',
-      image: '/img-post-topic-06.jpg',
-      alt: 'Imagem do post',
-      link: '#',
-    },
-  ];
+  postsRecentes: Post[] = [];
+
+  constructor() {
+    const generateId = (): string =>
+      `${Date.now()}-${Math.random().toString(36).substring(2, 9)}`;
+
+    this.postsRecentes = [
+      {
+        id: generateId(),
+        title: 'Realidade virtual: Entendendo a tecnologia',
+        date: '16, Fev',
+        readingTime: '12min',
+        category: 'Tecnologia',
+        image: '/img-post-topic-01.jpg',
+        alt: 'Imagem do post',
+        link: '#',
+      },
+      {
+        id: generateId(),
+        title: 'Você fica inseguro(a) na hora de escrever?',
+        date: '16, Fev',
+        readingTime: '12min',
+        category: 'Tecnologia',
+        image: '/img-post-topic-02.jpg',
+        alt: 'Imagem do post',
+        link: '#',
+      },
+      {
+        id: generateId(),
+        title: 'React Native Weekend - 21’ na minha visão',
+        date: '16, Fev',
+        readingTime: '12min',
+        category: 'Tecnologia',
+        image: '/img-post-topic-03.jpg',
+        alt: 'Imagem do post',
+        link: '#',
+      },
+      {
+        id: generateId(),
+        title: 'Como ir além apenas das linhas de código?',
+        date: '16, Fev',
+        readingTime: '12min',
+        category: 'Tecnologia',
+        image: '/img-post-topic-04.jpg',
+        alt: 'Imagem do post',
+        link: '#',
+      },
+      {
+        id: generateId(),
+        title: 'Como ir além apenas das linhas de código?',
+        date: '16, Fev',
+        readingTime: '12min',
+        category: 'Tecnologia',
+        image: '/img-post-topic-05.jpg',
+        alt: 'Imagem do post',
+        link: '#',
+      },
+      {
+        id: generateId(),
+        title: 'Você fica inseguro(a) na hora de escrever?',
+        date: '16, Fev',
+        readingTime: '12min',
+        category: 'Tecnologia',
+        image: '/img-post-topic-06.jpg',
+        alt: 'Imagem do post',
+        link: '#',
+      },
+    ];
+  }
 }
