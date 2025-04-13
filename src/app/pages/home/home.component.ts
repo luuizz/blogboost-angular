52a89f3119af8132e1d251d8ea4544605ce53b96
Luiz Ricardo <luu.izz@hotmail.com>
2025-04-13 14:51:45 -0300

    feat: adicionado componente de playlist

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeroComponent } from './sections/hero/hero.component';
 import { AllPostsComponent } from './sections/all-posts/all-posts.component';
 import { PodcastsComponent } from './sections/podcasts/podcasts.component';
+import { PlaylistsComponent } from "./sections/playlists/playlists.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroComponent, AllPostsComponent, PodcastsComponent],
+  imports: [HeroComponent, AllPostsComponent, PodcastsComponent, PlaylistsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
