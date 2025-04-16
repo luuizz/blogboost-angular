207afa3802e05473ec5d930e9d20b15b329ebcea
Luiz Ricardo <luu.izz@hotmail.com>
2025-04-16 20:26:17 -0300

    feat: construido parcialmente o layout e foi dado os primeiros fetches

--- src/app/pages/post/post.component.ts
+++ src/app/pages/post/post.component.ts
@@ -1,23 +1,77 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { inject } from '@angular/core';
-import { Observable, switchMap } from 'rxjs';
+import { gql } from 'graphql-request';
+import { hygraph } from 'utils/hygraph';
+import { PostResponse } from '@shared/interfaces/postResponse';
+import { GridComponent } from "../../components/grid/grid.component";
+import { CaptionComponent } from "../../components/caption/caption.component";
+import { PostHeaderActionsComponent } from "./post-header-actions/post-header-actions.component";
+import { PostMetaComponent } from "./post-meta/post-meta.component";
 
 @Component({
   selector: 'app-post',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, GridComponent, PostHeaderActionsComponent, CaptionComponent, PostMetaComponent],
   templateUrl: './post.component.html',
   styleUrl: './post.component.scss',
 })
-export class PostComponent {
-  title: string | null = null;
+export class PostComponent implements OnInit {
+  titulo: string | null = null;
   slug: string | null = null;
+  readingTime: number = 0;
+  isLoading = true;
+  post!: PostResponse['post'];
 
   constructor(private route: ActivatedRoute) {}
 
-  ngOnInit(): void {
+  async ngOnInit() {
     this.slug = this.route.snapshot.paramMap.get('slug');
+
+    if (!this.slug) {
+      this.isLoading = false;
+      return;
+    }
+
+      if (this.slug) {
+        const query = gql`
+        query GetPost($slug: String!) {
+          post(where: { slug: $slug }) {
+            slug
+            titulo
+            createdAt 
+            conteudoPost {
+              html
+              text
+            }
+          imagemDestacada {
+              width
+              height
+              url(transformation: {document: {output: {format: webp}}})
+            }
+          categoria {
+              nomeDaCategoria
+              slug
+            }
+          }
+        }
+      `;
+
+      try {
+        const data = await hygraph.request<PostResponse>(query, { slug: this.slug });
+        this.post = data.post;
+        this.readingTime = this.calculateReadingTime(this.post.conteudoPost[0].html || '');
+      } catch (e) {
+        console.error('Erro ao carregar post', e);
+      } finally {
+        this.isLoading = false;
+      }
+    }
+  }
+
+  calculateReadingTime(text: string): number {
+    const words = text.trim().split(/\s+/).length;
+
+    return Math.ceil(words / 200);
   }
 }
