ffcd8d5b17a99f17f63f1e2128518d493dca498f
Luiz Ricardo <luu.izz@hotmail.com>
2025-04-16 20:23:31 -0300

    feat: adicionado componente 404 e configurado rotas dinamicas de detalhamento de posts

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,17 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import { getAllSlugs } from 'utils/hygraph';
 
 export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
     renderMode: RenderMode.Prerender,
   },
+  {
+    path: 'post/:slug',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      const slugs = await getAllSlugs();
+      return slugs.map(slug => ({ slug }));
+    }
+  }
 ];
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,20 +3,25 @@ import { PostComponent } from './pages/post/post.component';
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './pages/home/home.component';
 import { ApplicationConfig } from '@angular/core';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
   },
-  // {
-  //   path: 'post/:slug',
-  //   component: PostComponent,
-  // },
+  {
+    path: 'post/:slug',
+    component: PostComponent,
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  }
 ];
 
-// export const appConfig: ApplicationConfig = {
-//   providers: [provideRouter(routes)],
-// };
+export const appConfig: ApplicationConfig = {
+  providers: [provideRouter(routes)],
+};
 
-// export class AppRountingModule {}
+export class AppRountingModule {}
