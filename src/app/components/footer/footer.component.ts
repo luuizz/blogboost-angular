b37623e98a0243cf808f0c8427be9dd872b3a1e5
Luiz Ricardo <luu.izz@hotmail.com>
2025-04-16 23:08:55 -0300

    feat: melhorado desempenho de imagens

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { GridComponent } from '../grid/grid.component';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [GridComponent],
+  imports: [GridComponent, NgOptimizedImage],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
