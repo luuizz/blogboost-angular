1c60a1aae03f54e63c0be297ce360fdac39d50e0
luuizz <luu.izz@hotmail.com>
2025-04-29 20:27:41 -0300

    feat: criado pagina 404

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { GridComponent } from "../../components/grid/grid.component";
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  imports: [GridComponent, NgOptimizedImage],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss'
 })
