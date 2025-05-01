04966d0d6f324d7e38b5af706a490653a22725b8
luuizz <luuizz@hotmail.com>
2025-05-01 12:06:01 -0300

    feat: criado component

---
+++ src/app/pages/categorias-index/categorias-index.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-categorias-index',
+  imports: [],
+  templateUrl: './categorias-index.component.html',
+  styleUrl: './categorias-index.component.scss'
+})
+export class CategoriasIndexComponent {
+
+}
