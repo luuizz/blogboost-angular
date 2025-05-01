1b7c0a4d15df19e7f979daf5d910ac569d8290c9
luuizz <luuizz@hotmail.com>
2025-05-01 12:04:17 -0300

    fix: adicionado url para tipagem

--- src/app/shared/interfaces/playlist.interface.ts
+++ src/app/shared/interfaces/playlist.interface.ts
@@ -6,6 +6,7 @@ export interface PlaylistItemWithMultipleImages {
     path: string;
     alt: string;
   }[];
+  url: string;
 }
 
 export interface PlaylistItemWithSingleImage {
@@ -13,6 +14,7 @@ export interface PlaylistItemWithSingleImage {
   title: string;
   image: string;
   alt: string;
+  url: string;
 }
 
 export type PlaylistItem = PlaylistItemWithMultipleImages | PlaylistItemWithSingleImage;
