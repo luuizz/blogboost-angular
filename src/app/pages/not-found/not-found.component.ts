548d709cfaa7ea24c9ff95e8764eab6228824f40
luuizz <luuizz@hotmail.com>
2025-05-03 13:13:19 -0300

    feat: adicionado componente de botão

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { GridComponent } from "../../components/grid/grid.component";
 import { NgOptimizedImage } from '@angular/common';
+import { ButtonComponent } from "../../shared/components/button/button.component";
 
 @Component({
   selector: 'app-not-found',
-  imports: [GridComponent, NgOptimizedImage],
+  imports: [GridComponent, NgOptimizedImage, ButtonComponent],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss'
 })
